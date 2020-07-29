b649a47a87e3700c84b012ca0e5de5606fc50c32
karonasrun.ks <karonasrun.ks@gmail.com>
2020-07-29 23:46:16 +0700

    Added localization in frontend & library for backend template

---
+++ resources/lang/kh/app.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'appName' => 'Portfolio',
+    'homePage' => 'ទំព័រដើម',
+    'servicePage' => 'សេវាកម្ម',
+    'portfolioPage' => 'ស្នាដៃរបស់ខ្ញូំ',
+    'aboutPage' => 'អំពីយើងខ្ញុំ',
+    'contactPage' => 'ទំនាក់ទំនង',
+    'usefulLinks' => 'តំណ​ភ្ជាប់​',
+    'termsofService' => 'លក្ខខណ្ឌ​នៃ​សេវាកម្ម',
+    'privacyPolicy' => 'គោលការណ៍​ភាព​ឯកជន',
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
